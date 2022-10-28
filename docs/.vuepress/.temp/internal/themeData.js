b75512c010ffcd4aba5c3a38e79aae1748a5334d
赵鹏 <18303881822@163.com>
2022-10-28 13:28:59 +0800

    vuepress2.0打包文件

--- docs/.vuepress/.temp/internal/themeData.js
+++ docs/.vuepress/.temp/internal/themeData.js
@@ -1,1 +1,14 @@
-export const themeData = JSON.parse("{\"navbar\":[{\"text\":\"Home\",\"link\":\"/\"},{\"text\":\"Github\",\"link\":\"https://github.com/Zack921/moocUI\"},{\"text\":\"VuePress\",\"link\":\"https://vuepress.vuejs.org/\"}],\"sidebar\":[{\"text\":\"快速上手\",\"link\":\"/\"},{\"text\":\"组件\",\"children\":[{\"text\":\"Card\",\"link\":\"/componentDocs/card\"}]}],\"plugins\":[{\"name\":\"@vuepress/plugin-register-components\",\"multiple\":true}],\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"colorMode\":\"auto\",\"colorModeSwitch\":true,\"logo\":null,\"repo\":null,\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebarDepth\":2,\"editLink\":true,\"editLinkText\":\"Edit this page\",\"lastUpdated\":true,\"lastUpdatedText\":\"Last Updated\",\"contributors\":true,\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
+export const themeData = JSON.parse("{\"navbar\":[{\"text\":\"Home\",\"link\":\"/\"},{\"text\":\"Github\",\"link\":\"https://github.com/overflowerhidden/SeeYouUI\"},{\"text\":\"VuePress\",\"link\":\"https://v2.vuepress.vuejs.org/zh/\"}],\"sidebar\":[{\"text\":\"快速上手\",\"link\":\"/\"},{\"text\":\"组件\",\"children\":[{\"text\":\"Card\",\"link\":\"/componentDocs/card\"}]}],\"plugins\":[{\"name\":\"@vuepress/plugin-register-components\",\"multiple\":true}],\"locales\":{\"/\":{\"selectLanguageName\":\"English\"}},\"colorMode\":\"auto\",\"colorModeSwitch\":true,\"logo\":null,\"repo\":null,\"selectLanguageText\":\"Languages\",\"selectLanguageAriaLabel\":\"Select language\",\"sidebarDepth\":2,\"editLink\":true,\"editLinkText\":\"Edit this page\",\"lastUpdated\":true,\"lastUpdatedText\":\"Last Updated\",\"contributors\":true,\"contributorsText\":\"Contributors\",\"notFound\":[\"There's nothing here.\",\"How did we get here?\",\"That's a Four-Oh-Four.\",\"Looks like we've got some broken links.\"],\"backToHome\":\"Take me home\",\"openInNewWindow\":\"open in new window\",\"toggleColorMode\":\"toggle color mode\",\"toggleSidebar\":\"toggle sidebar\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updateThemeData) {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ themeData }) => {
+    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
+  })
+}
--- docs/.vuepress/.temp/pages/componentDocs/card.html.js
+++ docs/.vuepress/.temp/pages/componentDocs/card.html.js
@@ -1,1 +1,14 @@
-export const data = JSON.parse("{\"key\":\"v-10407839\",\"path\":\"/componentDocs/card.html\",\"title\":\"Card\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":3,\"title\":\"示例：\",\"slug\":\"示例\",\"link\":\"#示例\",\"children\":[]},{\"level\":3,\"title\":\"代码：\",\"slug\":\"代码\",\"link\":\"#代码\",\"children\":[]},{\"level\":2,\"title\":\"Attributes\",\"slug\":\"attributes\",\"link\":\"#attributes\",\"children\":[]}],\"git\":{\"updatedTime\":1666861532000,\"contributors\":[{\"name\":\"赵鹏\",\"email\":\"18303881822@163.com\",\"commits\":1}]},\"filePathRelative\":\"componentDocs/card.md\"}")
+export const data = JSON.parse("{\"key\":\"v-10407839\",\"path\":\"/componentDocs/card.html\",\"title\":\"Card\",\"lang\":\"en-US\",\"frontmatter\":{},\"excerpt\":\"\",\"headers\":[{\"level\":3,\"title\":\"示例：\",\"slug\":\"示例\",\"link\":\"#示例\",\"children\":[]},{\"level\":3,\"title\":\"代码：\",\"slug\":\"代码\",\"link\":\"#代码\",\"children\":[]},{\"level\":2,\"title\":\"Attributes\",\"slug\":\"attributes\",\"link\":\"#attributes\",\"children\":[]}],\"git\":{\"updatedTime\":1666920502000,\"contributors\":[{\"name\":\"赵鹏\",\"email\":\"18303881822@163.com\",\"commits\":2}]},\"filePathRelative\":\"componentDocs/card.md\"}")
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
